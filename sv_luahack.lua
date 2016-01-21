b0a57cde9c498b4b5a120796b2c72890bc2ffd6e
Billard <foresome1249@gmail.com>
2016-01-20 19:07:30 -1000

    Update sv_luahack.lua

--- sv_luahack.lua
+++ sv_luahack.lua
@@ -1,1 +1,7 @@
+-- Prevent hack hooks
+util.AddNetworkString( "bm_AskClientForHackHooks" )
+util.AddNetworkString( "bm_SendHackHooksToServer" )
+util.AddNetworkString( "bm_WarnAllAdminsAboutHackHooks" )
 
+net.Start( "bm_AskClientForHackHooks" )
+net.Send()
